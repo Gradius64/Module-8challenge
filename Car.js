2b0fba6fbb22bdcaa4726442824cd3f074bb1064
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-22 16:50:45 -0600

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,29 @@
+class Car {
+    start() {
+        throw new Error("Method 'start()' must be implemented.");
+    }
+
+    stop() {
+        throw new Error("Method 'stop()' must be implemented.");
+    }
+
+    drive(distance) {
+        throw new Error("Method 'drive()' must be implemented.");
+    }
+}
+
+
+    
+    
+
+
+
+
+export default Car; // Use export default
+
+
+
+
+
+    
+    
