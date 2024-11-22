d2dbe076c48ebbafe6105d01a729fb0301ac1d06
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-11-22 16:50:05 -0600

    Add files via upload

---
+++ Driveable.js
@@ -0,0 +1,14 @@
+class Driveable {
+    start() {
+        throw new Error("Method 'start()' must be implemented.");
+    }
+
+    stop() {
+        throw new Error("Method 'stop()' must be implemented.");
+    }
+
+    drive(distance) {
+        throw new Error("Method 'drive()' must be implemented.");
+    }
+}
+
